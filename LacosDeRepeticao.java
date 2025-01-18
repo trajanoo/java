0c15970bd4204586011bf3edaf4854b9c5049a9b
trajanoo <brunotrajano.dev@gmail.com>
2025-01-18 16:46:10 -0300

    Add files via upload

---
+++ LacosDeRepeticao.java
@@ -0,0 +1,18 @@
+package Condicoes;
+
+import java.util.Scanner;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um número inteiro positivo: ");
+        int numero = input.nextInt();
+
+        input.close();
+
+        for(int i = 1; i <= numero; i++) {
+            String parOuImpar = i % 2 == 0 ? " é par" : " é impar";
+            System.out.println("Número " + i + parOuImpar);
+        }
+    }
+}
