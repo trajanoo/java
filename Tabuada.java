4c92b7d8b7aa12d8e6dd45a1b2762cb1103ed1ec
trajanoo <brunotrajano.dev@gmail.com>
2025-01-19 19:50:13 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,17 @@
+package exIniciante;
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Visualizar tabuada do número: ");
+        int num = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+            int resultado = num * i;
+            System.out.println(num + " x " + i + " = " + resultado);
+        }
+    }
+}
