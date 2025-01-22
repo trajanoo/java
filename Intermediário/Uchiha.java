fa31835729ea899ca759f71b21578617462cd6a7
trajanoo <brunotrajano.dev@gmail.com>
2025-01-22 00:16:13 -0300

    Delete Intermediário/Uchiha.java

--- Intermediário/Uchiha.java
+++
@@ -1,9 +0,0 @@
-package Intermediário;
-
-public class Uchiha extends Ninja {
-
-    @Override
-    public void habilidadeEspecial() {
-        System.out.println("Eu sou um uchiha e meu ataque especial é o sharingan");
-    }
-}
